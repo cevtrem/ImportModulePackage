827dc0f6fb1c1714e8dd7763988e71b6a87222d4
Mladinsky <cevtrem@gmail.com>
2025-02-21 20:24:02 +0300

    Create Package

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime
-from people import get_employees
-from salary import calculate_salary
+from application.db.people import get_employees
+from application.salary import calculate_salary
 from colorful_print import color
 
 b = calculate_salary()
